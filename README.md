f1c0cc6a6064eb58dba22ad0b1c1564edd06a407
Petar Petrov <pepetrov@gmail.com>
2011-03-16 10:31:08 +0100

    initial commit, adding project stub

---
+++ README.md
@@ -0,0 +1,2 @@
+Information Retrieval - Text Indexer
+=====
---
+++ src/main/java/at/tuwien/ir/textindexer/App.java
@@ -0,0 +1,13 @@
+package at.tuwien.ir.textindexer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
